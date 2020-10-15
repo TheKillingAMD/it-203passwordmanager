50a95b18164ce23646a09ac216b973fd6281a856
Ayush Mangukia <54385325+TheKillingAMD@users.noreply.github.com>
2020-10-15 10:14:31 +0400

    Login and Registeration

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e6040f19d063c7ea55a33765df99277c'
 
 @app.route("/")
 def homepage():
     return render_template("home.html")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
